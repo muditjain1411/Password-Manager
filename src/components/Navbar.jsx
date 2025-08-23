f4bd5672fbb93c25c313f4b812509c97270ea6d2
Mudit Jain <Muditjain.1406@gmail.com>
2025-08-24 00:30:02 +0530

    bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Logo from '../assets/logo.png';
+import logo from '../assets/logo.png';
 const Navbar = ({ setShowModal }) => {
 
     return (
         <>
             <div className="flex flex-row justify-between items-center gap-4 px-8 py-3 fixed top-0 left-0 w-full bg-gradient-to-r from-gray-900 via-gray-800 to-gray-700 shadow-xl rounded-b-2xl z-50">
                 <div className="flex flex-row items-center gap-4">
-                    <img src={Logo} className="w-12 h-12 rounded-xl shadow-md" alt="Logo" />
+                    <img src={logo} className="w-12 h-12 rounded-xl shadow-md" alt="Logo" />
                     <div className="text-3xl font-extrabold font-[cursive] tracking-wide text-white drop-shadow-lg select-none">PASSVAULT</div>
                 </div>
                 <div className='flex flex-row items-center gap-4 p-2'>
